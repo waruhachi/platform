0fcfc8d2a39988c55ec676e23841ed1fc3cbbe01
Pedro Figueiredo <pedro@neon.tech>
2025-03-25 11:08:21 +0000

    feat: remove dist useffect

--- app-build-cli/dist/app.js
+++ app-build-cli/dist/app.js
@@ -1,12 +1,7 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { useEffect } from 'react';
+import { jsx as _jsx } from 'react/jsx-runtime';
 import { ChatBotFlow } from './chatbot/create-chatbot.js';
 // refresh the app every 100ms
-const useKeepAlive = () => useEffect(() => {
-    setInterval(() => { }, 100);
-}, []);
 export const App = () => {
-    useKeepAlive();
-    return _jsx(ChatBotFlow, {});
+  return _jsx(ChatBotFlow, {});
 };
 //# sourceMappingURL=app.js.map
