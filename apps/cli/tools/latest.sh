62047afae8818c2e89b83722e78d118b12211c02
Mario Cadenas <MarioCadenas@users.noreply.github.com>
2025-06-09 20:49:53 +0200

    chore: brew bump (#186)

--- apps/cli/tools/latest.sh
+++ apps/cli/tools/latest.sh
@@ -5,3 +5,5 @@ LATEST_BETA=$(npm view "$PACKAGE_NAME" dist-tags.beta)
 
 echo "Promoting $PACKAGE_NAME@$LATEST_BETA to latest"
 npm dist-tag add "$PACKAGE_NAME@$LATEST_BETA" latest
+
+brew bump-formula-pr --version="$LATEST_BETA" --no-fork --no-browser appdotbuild/tap/app.build
